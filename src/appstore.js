83bdcbab62e0112b24eb21463144a10e0f252e1d
yap cheah shen <yapcheahshen@gmail.com>
2023-12-03 10:54:44 +0800

    add image

---
+++ src/appstore.js
@@ -0,0 +1,18 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::chengguan',
+_ACC23.AppVer='23.10.31';
+_ACC23.AppName='chengguan'
+_ACC23.AppTitle='成觀法師'
+_ACC23.allptks=['chengguan','chengguan-en'];
+_ACC23.selectedptks=['chengguan','chengguan-en']
+_ACC23.activeptk='chengguan'
+_ACC23.ptkinfo={
+    'chengguan':"中",
+    'chengguan-en':"英",
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
